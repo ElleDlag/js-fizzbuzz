183609eba23ee6aec9ff69dd0cc7a569ebacaa39
ElleDlag <38381770+ElleDlag@users.noreply.github.com>
2019-10-24 15:30:46 +0200

    base version ok

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+//Number.isInteger() method ecma 2015
 
+for(var i = 1; i <= 100; i++){
+    if(Number.isInteger(i / 3) && !Number.isInteger(i / 5)){
+        console.log("Fizz");
+    } else if(Number.isInteger(i / 5) && !Number.isInteger(i / 3)){
+        console.log("Buzz");
+    } else if(Number.isInteger(i / 3) &&  Number.isInteger(i / 5)){
+        console.log("FizzBuzz");
+    } else (console.log(i))
+}
