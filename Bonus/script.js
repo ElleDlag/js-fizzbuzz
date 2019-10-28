a04368c3dd938f479e4071e2563168d7d76b5cba
ElleDlag <38381770+ElleDlag@users.noreply.github.com>
2019-10-28 09:51:20 +0100

    Bonus

--- Bonus/script.js
+++ Bonus/script.js
@@ -1,5 +1,5 @@
 //superQuickly short LCM with any number
-function fizzBuzz(a,b){
+/* function fizzBuzz(a,b){
     var n=1;
    while(n<100){
     switch (true){
@@ -23,4 +23,20 @@ function fizzBuzz(a,b){
     n++
 }
 }
-fizzBuzz(3,5);
+fizzBuzz(3,5); */
+
+//pensa come simone
+var label;
+for(var i = 1 ; i<100 ; i++ ){
+
+    if( !(i % 3) || !(i % 5)){
+        label="";
+        if(!i % 3){
+            console.log(label += "Fizz");
+        }
+        if(!i % 5){
+            console.log(label += "Buzz");
+        }
+    }
+    console.log(i)
+}
